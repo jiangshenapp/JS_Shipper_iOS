d5b7378848d2cbca6b3c29adbe7c2d10e8e94d0c
zyl <zengyulu@evcoming.com>
2019-03-26 18:05:43 +0800

    我的钱包

---
+++ JS_Shipper/ViewController/Mine/JSMyWalletVC.m
@@ -0,0 +1,16 @@
+//
+//  JSMyWalletVC.m
+//  JS_Shipper
+//
+//  Created by zhanbing han on 2019/3/26.
+//  Copyright © 2019年 zhanbing han. All rights reserved.
+//
+
+#import "JSMyWalletVC.h"
+
+@implementation JSMyWalletVC
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"我的钱包";
+}
+@end
