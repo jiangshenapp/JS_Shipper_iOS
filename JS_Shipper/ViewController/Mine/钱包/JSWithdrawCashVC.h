f7d7ad323ca22b5c262a11f23f3678ae02b70206
zyl <zengyulu@evcoming.com>
2019-03-26 21:06:15 +0800

    申请提现页面搭建

---
+++ JS_Shipper/ViewController/Mine/钱包/JSWithdrawCashVC.h
@@ -0,0 +1,17 @@
+//
+//  JSWithdrawCashVC.h
+//  JS_Shipper
+//
+//  Created by zhanbing han on 2019/3/26.
+//  Copyright © 2019年 zhanbing han. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSWithdrawCashVC : BaseVC
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JS_Shipper/ViewController/Mine/钱包/JSWithdrawCashVC.m
@@ -0,0 +1,33 @@
+//
+//  JSWithdrawCashVC.m
+//  JS_Shipper
+//
+//  Created by zhanbing han on 2019/3/26.
+//  Copyright © 2019年 zhanbing han. All rights reserved.
+//
+
+#import "JSWithdrawCashVC.h"
+
+@interface JSWithdrawCashVC ()
+
+@end
+
+@implementation JSWithdrawCashVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = @"提现";
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
