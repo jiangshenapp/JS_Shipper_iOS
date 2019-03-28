d7bd8fbc93257353a0819eb3e385e1e7bd2db96c
zyl <zengyulu@evcoming.com>
2019-03-28 11:16:33 +0800

    发布中订单vc建立

---
+++ JS_Shipper/ViewController/Mine/我的订单/JSOrderReleasingVC.h
@@ -0,0 +1,17 @@
+//
+//  JSOrderReleasingVC.h
+//  JS_Shipper
+//
+//  Created by zhanbing han on 2019/3/28.
+//  Copyright © 2019年 zhanbing han. All rights reserved.
+//
+
+#import "BaseVC.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JSOrderReleasingVC : BaseVC
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JS_Shipper/ViewController/Mine/我的订单/JSOrderReleasingVC.m
@@ -0,0 +1,32 @@
+//
+//  JSOrderReleasingVC.m
+//  JS_Shipper
+//
+//  Created by zhanbing han on 2019/3/28.
+//  Copyright © 2019年 zhanbing han. All rights reserved.
+//
+
+#import "JSOrderReleasingVC.h"
+
+@interface JSOrderReleasingVC ()
+
+@end
+
+@implementation JSOrderReleasingVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
