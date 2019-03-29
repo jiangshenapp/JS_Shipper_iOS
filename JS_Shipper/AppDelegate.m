9d79ea032e61ff3bade3a4b6739f4a336dabc058
zyl <zengyulu@evcoming.com>
2019-03-29 15:07:17 +0800

    解决tabbar上移

--- JS_Shipper/AppDelegate.m
+++ JS_Shipper/AppDelegate.m
@@ -17,6 +17,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    [[UITabBar appearance] setTranslucent:NO];//解决tabbar上移
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
     self.window.rootViewController = [[BaseTabBarVC alloc] init];
 //    self.window.rootViewController = [[UINavigationController alloc]initWithRootViewController:[Utils getViewController:@"Login" WithVCName:@"JSPaswdLoginVC"]];
