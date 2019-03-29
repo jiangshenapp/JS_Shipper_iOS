4d7e13b304aa047fcd0e29e451d70224667ddcda
zyl <zengyulu@evcoming.com>
2019-03-29 15:29:50 +0800

    支付页面UI搭建 用户中心UI创建

---
+++ JS_Shipper/ViewController/Mine/JSMyInfoVC.m
@@ -0,0 +1,32 @@
+//
+//  JSMyInfoVC.m
+//  JS_Shipper
+//
+//  Created by zhanbing han on 2019/3/29.
+//  Copyright © 2019年 zhanbing han. All rights reserved.
+//
+
+#import "JSMyInfoVC.h"
+
+@interface JSMyInfoVC ()
+
+@end
+
+@implementation JSMyInfoVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
