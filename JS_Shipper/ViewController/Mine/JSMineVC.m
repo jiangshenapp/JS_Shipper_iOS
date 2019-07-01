54611ffbc9b99cf4c938434644da6c2babf8f212
hanzhanbing <1655661337@qq.com>
2019-07-01 17:36:57 +0800

    每次进入我的都刷新接口

--- JS_Shipper/ViewController/Mine/JSMineVC.m
+++ JS_Shipper/ViewController/Mine/JSMineVC.m
@@ -26,9 +26,7 @@
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
     
-    if ([Utils isLoginWithJump:YES]) {
-        [self getAccountInfo]; //获取账户信息
-    }
+    [self getData];
 }
 
 - (void)viewDidLoad {
@@ -36,10 +34,10 @@
     
     self.navBar.hidden = YES;
     
-    [self getData];
-    
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getData) name:kLoginSuccNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getData) name:kUserInfoChangeNotification object:nil];
+//    [self getData];
+//    
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getData) name:kLoginSuccNotification object:nil];
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getData) name:kUserInfoChangeNotification object:nil];
 //    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(getData) name:kChangeMoneyNotification object:nil];
     
     iconArr = @[@"personalcenter_icon_park",@"personalcenter_icon_service",@"personalcenter_icon_invoice",@"personalcenter_icon_collection",@"personalcenter_icon_customer"];
@@ -85,7 +83,7 @@
     
     if ([Utils isLoginWithJump:YES]) {
         [self getUserInfo]; //获取用户信息
-//        [self getAccountInfo]; //获取账户信息
+        [self getAccountInfo]; //获取账户信息
     }
 }
 
